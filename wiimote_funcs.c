b93e341951267de2a56768c3641c02e381394081
Will Ross <paxswill@gmail.com>
2010-04-19 04:23:25 -0400

    Pulled the wiimote function out
    Pulled the connect function into a separate file, also adding a header while I was at it

--- wiimote_funcs.c
+++ wiimote_funcs.c
@@ -1,1 +1,20 @@
+#include "wiimote_funcs.h"
+
 //Various wiimote functions
+wiimote** findWiimote(){
+	//where to store the wiimotes
+	int maxWiimotes = 1;
+	wiimote** wm = wiiuse_init(maxWiimotes);
+	
+	//find wiimotes
+	int numFound = wiiuse_find(wm, maxWiimotes, 5);
+	
+	//connect to the wiimote(s)
+	if(wiiuse_connect(wm, maxWiimotes)){
+		printf("Connected to wiimote. %i found\n", numFound);
+		return wm;
+	}
+	else{
+		return 0;
+	}
+}
---
+++ wiimote_funcs.h
@@ -0,0 +1,9 @@
+#ifndef GESTURE_WIIMOTE
+#define GESTURE_WIIMOTE
+
+#include <wiiuse.h>
+
+wiimote** findWiimote();
+
+
+#endif /* end of include guard: GESTURE_WIIMOTE */
