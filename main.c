bf64e275a21beef8710994e37ff0ef2d42f23487
Will Ross <paxswill@gmail.com>
2010-04-19 04:25:59 -0400

    Still pulling wiimote funcs out

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <wiiuse.h>
-
-wiimote** findWiimote();
+#include "wiimote_funcs.h"
 
 int main (int argc, char const *argv[])
 {
@@ -13,20 +11,3 @@ int main (int argc, char const *argv[])
 	return 0;
 }
 
-wiimote** findWiimote(){
-	//where to store the wiimotes
-	int maxWiimotes = 1;
-	wiimote** wm = wiiuse_init(maxWiimotes);
-	
-	//find wiimotes
-	int numFound = wiiuse_find(wm, maxWiimotes, 5);
-	
-	//connect to the wiimote(s)
-	if(wiiuse_connect(wm, maxWiimotes)){
-		printf("Connected to wiimote. %i found\n", numFound);
-		return wm;
-	}
-	else{
-		return 0;
-	}
-}
