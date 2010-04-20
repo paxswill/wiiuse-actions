451f918a6b057f076400ac3a8ffa8dd95f500830
Will Ross <paxswill@gmail.com>
2010-04-20 03:45:54 -0400

    Added buttons to the cell data

--- buffer.h
+++ buffer.h
@@ -8,6 +8,22 @@ typedef struct cell_t{
 	double x_accel;
 	double y_accel;
 	double z_accel;
+	short buttons;
+	/*
+	The above variable is a packed representation of the state of 
+	all 11 buttons on a wiimote. In order:
+	0x0001	Up
+	0x0002	Left
+	0x0004	Down
+	0x0008	Right
+	0x0010	A
+	0x0020	B
+	0x0040	Minus
+	0x0080	Home
+	0x0100	Plus
+	0x0200	1
+	0x0400	2
+	*/
 } cell;
 
 typedef struct queue_t{
